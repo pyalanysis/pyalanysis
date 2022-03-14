51fb4d014467052eaab20268fa7b7bcbc827891c
Anton Bossenbroek <anton.bossenbroek@gmail.com>
2022-03-14 16:12:38 +0100

    Adds initial VIIRS DNB loader

---
+++ src/pyalanysis/data/__init__.py
@@ -0,0 +1,13 @@
+from .viirsdnb import (
+    get_viirs_dnb_monthly,
+    get_viirs_dnb_monthly_fn,
+    ViirsDnbMonthly,
+    ViirsDnbMonthlyType,
+)
+
+__all__ = [
+    "ViirsDnbMonthlyType",
+    "ViirsDnbMonthly",
+    "get_viirs_dnb_monthly",
+    "get_viirs_dnb_monthly_fn",
+]
