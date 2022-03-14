f2949289231d00ca8ad8efdc5fa4b8074b9f5731
Anton Bossenbroek <anton.bossenbroek@gmail.com>
2022-03-14 10:40:47 +0100

    Fixes formatting

--- docs/conf.py
+++ docs/conf.py
@@ -5,5 +5,10 @@ from datetime import datetime
 project = "Pyalanysis"
 author = "Anton Bossenbroek"
 copyright = f"{datetime.now().year}, {author}"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints", "myst_parser"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "myst_parser",
+]
 html_static_path = ["_static"]
