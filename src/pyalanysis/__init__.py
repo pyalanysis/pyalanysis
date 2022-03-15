007a60120c38235e2472e7384c9f57b3d4288100
Anton Bossenbroek <anton.bossenbroek@gmail.com>
2022-03-15 18:11:12 +0100

    Improves logging

--- src/pyalanysis/__init__.py
+++ src/pyalanysis/__init__.py
@@ -2,8 +2,7 @@ import logging
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
-formatter = logging.Formatter(
-    '%(asctime)s %(name)-25s %(levelname)-8s %(message)s')
+formatter = logging.Formatter("%(asctime)s %(name)-25s %(levelname)-8s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
