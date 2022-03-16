96acf80687eb966aec3ac7a1f50c0c88fb2ed29b
Anton Bossenbroek <anton.bossenbroek@gmail.com>
2022-03-16 13:37:24 +0100

    Completes unit testing of utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,26 +2,24 @@ import os
 import shutil
 import tempfile
 from typing import cast
+from pathlib import Path
 from unittest import mock, TestCase
 from unittest.mock import Mock
 
 from pyalanysis.utils import ensure_cache_dir, get_cache_dir  # type: ignore
 
+_tempdir: str = (tempfile.tempdir or "/tmp") + "/test_pyalanysis"
 
 class TestGetCacheDir(TestCase):
-    _tempdir: str = (tempfile.tempdir or "/tmp") + "/test_pyalanysis"
 
     def setUp(self):
         if os.name == "posix":
-            if os.path.exists(self._tempdir):
-                shutil.rmtree(self._tempdir)
+            if os.path.exists(_tempdir):
+                shutil.rmtree(_tempdir)
 
-    @mock.patch.dict(
-        os.environ,
-        {"PYALANYSIS_MINES_USERNAME": ""},
-    )
     @mock.patch("os.path.expanduser", mock.Mock(return_value=_tempdir))
     @mock.patch("os.makedirs", mock.Mock(return_value=0))
+    @mock.patch("sys.platform", "linux")
     def test_get_cache_dir(self) -> None:
         if os.name == "posix":
             get_cache_dir()
@@ -33,24 +31,24 @@ class TestGetCacheDir(TestCase):
             assert not makedirs.called
 
     @mock.patch("os.path.expanduser", mock.Mock(return_value=_tempdir))
-    @mock.patch("sys.platform", "Darwin")
+    @mock.patch("sys.platform", "darwin")
     def test_get_cache_dir_mac(self) -> None:
         get_cache_dir()
 
         mock_expanduser = cast(Mock, os.path.expanduser)
         assert mock_expanduser.called
 
-    @mock.patch.dict(
-        os.environ,
-        {"PYALANYSIS_MINES_USERNAME": ""},
-    )
+
     @mock.patch("os.path.expanduser", mock.Mock(return_value=_tempdir))
     @mock.patch("sys.platform", "win64")
-    def test_get_cache_dir_mac(self) -> None:
-        get_cache_dir()
+    @mock.patch("os.name", "win64")
+    def test_get_cache_dir_win(self) -> None:
+        cache_dir = get_cache_dir()
 
         mock_expanduser = cast(Mock, os.path.expanduser)
         assert mock_expanduser.called
+        assert cache_dir == Path("/tmp/test_pyalanysis/pyalanysis")
+
 
 
 class TestEnsureDir(TestCase):
@@ -58,13 +56,9 @@ class TestEnsureDir(TestCase):
 
     def setUp(self):
         if os.name == "posix":
-            if os.path.exists(self._tempdir):
-                shutil.rmtree(self._tempdir)
+            if os.path.exists(_tempdir):
+                shutil.rmtree(_tempdir)
 
-    @mock.patch.dict(
-        os.environ,
-        {"PYALANYSIS_MINES_USERNAME": ""},
-    )
     @mock.patch("os.path.expanduser", mock.Mock(return_value=_tempdir))
     def test_ensure_cache_dir_new_dir(self) -> None:
         if os.name == "posix":
@@ -73,12 +67,9 @@ class TestEnsureDir(TestCase):
             mock_expanduser = cast(Mock, os.path.expanduser)
 
             assert mock_expanduser
-            assert os.path.exists(self._tempdir)
+            assert os.path.exists(_tempdir)
 
-    @mock.patch.dict(
-        os.environ,
-        {"PYALANYSIS_MINES_USERNAME": ""},
-    )
+
     @mock.patch("os.path.expanduser", mock.Mock(return_value=_tempdir))
     def test_ensure_cache_dir_existing_dir(self) -> None:
         if os.name == "posix":
@@ -90,9 +81,9 @@ class TestEnsureDir(TestCase):
             mock_expanduser = cast(Mock, os.path.expanduser)
 
             assert mock_expanduser
-            assert os.path.exists(self._tempdir)
+            assert os.path.exists(_tempdir)
 
     def tearDown(self) -> None:
         if os.name == "posix":
-            if os.path.exists(self._tempdir):
-                shutil.rmtree(self._tempdir)
+            if os.path.exists(_tempdir):
+                shutil.rmtree(_tempdir)
